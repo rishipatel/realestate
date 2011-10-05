6001d02025a73c9e89aad8d4817be4ad24f1f1b0
Rishi Patel <ank23rishi@gmail.com>
2011-10-05 23:03:19 +0530

    few routes

--- init.php
+++ init.php
@@ -1,15 +1,59 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('realestate/blog', 'realestate/blog(/<action>)')
-	->defaults(array(
-		'directory' => 'realestate',
-		'controller' => 'blog',
-		'action'     => 'index',
-	));
+Route::set('auth', '<action>',
+  array(
+    'action' => '(login|logout|register)'
+  ))
+  ->defaults(array(
+    'controller' => 'user'
+  ));
+
+// Route::set('signin', 'realestate/blog(/<action>)')
+	// ->defaults(array(
+		// 'directory' => 'realestate',
+		// 'controller' => 'blog',
+		// 'action'     => 'index',
+	// ));
 	
-Route::set('realestate/admin', 'realestate/admin(/<action>)')
+// Route::set('realestate/admin', 'realestate/admin(/<action>)')
+	// ->defaults(array(
+		// 'directory' => 'realestate',
+		// 'controller' => 'admin',
+		// 'action'     => 'index',
+	// ));
+	
+// Route::set('realestate/home', 'realestate')
+	// ->defaults(array(
+		// 'controller' => 'home',
+		// 'action'     => 'index',
+	// ));
+
+// Route::set('realestate/auth/signup', 'realestate/signup(/<action>)')
+	// ->defaults(array(
+		// 'directory' => 'realestate',
+		// 'directory' => 'auth',
+		// 'controller' => 'user',
+		// 'action'     => 'signup',
+	// ));
+
+// Route::set('realestate/auth/signin', 'realestate/signin(/<action>)')
+	// ->defaults(array(
+		// 'directory' => 'realestate',
+		// 'directory' => 'auth',
+		// 'controller' => 'user',
+		// 'action'     => 'signin',
+	// ));
+	
+/***********New Paths************/
+// Route::set('realestate/user/login', 'signin(/<action>)')
+	// ->defaults(array(
+		// 'directory' => 'realestate',
+		// 'controller' => 'user',
+		// 'action'     => 'login',
+	// ));
+	
+Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'directory' => 'realestate',
-		'controller' => 'admin',
+		'controller' => 'home',
 		'action'     => 'index',
 	));
